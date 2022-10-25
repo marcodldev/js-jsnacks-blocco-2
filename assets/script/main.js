5784725aa26cccaad9818a8d27489d0f26800b7c
marcodldev <marcodelisi2@gmail.com>
2022-10-25 16:45:01 +0200

    + snack 3 ok

--- assets/script/main.js
+++ assets/script/main.js
@@ -17,5 +17,31 @@ if (numero % 2 == 0 ) {
 // Generatore di “nomi cognomi” casuali del grande gatsby:
 // Abbiamo una lista di nomi e una lista di cognomi, e da queste vogliamo generare una "falsa" lista di invitati, dove ogni invitato nella lista, è l'unione di un "nome" e un "cognome" delle liste precedentemente dette, scelti in modo casuale. 
 
-let listaNomi = [ilario,marco,giovanni,francesco,luca];
-let listaCognomi = [rossi,verdi,gialli,rossi,violi];
+let listaNomi = ["ilario","marco","giovanni","francesco","luca"];
+let listaCognomi = ["rossi","verdi","gialli","rossi","violi"];
+
+
+let nomeRandom = Math.floor(Math.random() * listaNomi.length);
+let cognomeRandom = Math.floor(Math.random() * listaCognomi.length);
+
+console.log(`${nomeRandom, listaNomi[nomeRandom]} ${cognomeRandom, listaCognomi[cognomeRandom]}`);
+
+
+//FUNZIONA 
+
+// *Snack3*
+// Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione (indice) dispari
+
+let somma = 0;
+
+let numeriInteri = [1,2,3,4,5,6,7,8,9,10];
+
+    for (let i = 0; i < numeriInteri.length; i++) {
+        if (i % 2 != 0 ) {
+        somma = somma + numeriInteri[i]; 
+    }
+}
+
+console.log(somma);
+
+  //FUNZIONA
